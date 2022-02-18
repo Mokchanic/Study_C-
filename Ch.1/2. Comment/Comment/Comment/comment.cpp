eaf8b48fef89f34f7ca32167abddd1ab2931f819
MokJinWoo_Player <63028144+Mokchanic@users.noreply.github.com>
2022-02-18 19:00:32 +0900

    update_comment

---
+++ Ch.1/2. Comment/Comment/Comment/comment.cpp
@@ -0,0 +1,29 @@
+/*
+* This is my program.
+* Written by me.
+*/
+
+#include <iostream>
+
+int main(void)
+{
+	int x = 2;
+	x = 5;
+	int y = x + 3;
+
+	std::cout << y << std::endl;
+
+	int sight = 10;
+
+	// ...
+
+
+	//몬스터한테 저주가 걸려서 시야 거리가 0
+	//하지만, 코드만 보고 알 수 있다면, 기록을 할 필요가 없음.
+	sight = 0;
+
+	// 1 + 1 event
+	//int cost = n_items / 2 * price_per_item;
+
+	return 0;
+}
